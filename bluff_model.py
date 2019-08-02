87e3026dda222f05dad28f5078fde0ede425dcb1
Siddharth M <siddharth.m98@gmail.com>
2019-08-02 14:40:23 +0530

    Polynomial regression for bluff detection

--- bluff_model.py
+++ bluff_model.py
@@ -3,6 +3,44 @@
 """
 Created on Fri Aug  2 14:29:41 2019
 
-@author: root
+@author: siddharth.m98@gmail.com
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, 0:1].values
+y = dataset.iloc[:, 1].values
+
+
+from sklearn.linear_model import LinearRegression
+lin_reg  = LinearRegression()
+lin_reg.fit(X,y)
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 5)
+X_poly = poly_reg.fit_transform(X)
+
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly,y)
+
+plt.scatter(X,y,color = 'red')
+plt.plot(X,lin_reg.predict(X),color = 'blue')
+plt.title('truth or bluff - linear reg')
+plt.xlabel('Experience level')
+plt.ylabel('salary')
+plt.show()
+
+
+
+plt.scatter(X,y,color = 'red')
+plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)),color = 'blue')
+plt.title('truth or bluff - linear reg')
+plt.xlabel('Experience level')
+plt.ylabel('salary')
+plt.show()
+
+lin_reg2.predict(poly_reg.fit_transform([[6.5]]))
